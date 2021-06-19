d5a46c13470c280b597cde31af7f4186e9693bbc
Rocky <rockygitt@gmail.com>
2021-06-20 01:49:03 +0200

    Fixes

--- build.sh
+++ build.sh
@@ -1,31 +1,34 @@
 #!/usr/bin/env bash
 
 package_name=$1
-arch_name=${2/-/\/}
 
-if [[ -z "$package_name" ]] || [[ -z "$arch_name" ]]; then
-  echo "usage: $0 <package-name> <arch-name>"
+if [[ -z "$package_name" ]]; then
+  echo "usage: $0 <package-name>"
   exit 1
 fi
 
-arch_split=(${arch_name//\// })
+archs=('darwin/amd64' 'darwin/arm64' 'linux/amd64' 'linux/amr64' 'windows/amd64')
 
-GOOS=${arch_split[0]}
-GOARCH=${arch_split[1]}
+for arch_name in "${archs[@]}"; do
+  arch_split=(${arch_name//\// })
 
-zip_filename="${package_name}-${GOOS}-${GOARCH}.zip"
+  GOOS=${arch_split[0]}
+  GOARCH=${arch_split[1]}
 
-if [ $GOOS = 'windows' ]; then
-  package_name+='.exe'
-fi
+  zip_filename="${package_name}-${GOOS}-${GOARCH}.zip"
 
-env GOOS=$GOOS GOARCH=$GOARCH go build --ldflags='-s -w' -o $package_name *.go
-upx --ultra-brute $package_name &>/dev/null
+  if [ $GOOS = 'windows' ]; then
+    package_name+='.exe'
+  fi
 
-if [ $? -ne 0 ]; then
-  echo 'An error has occurred! Aborting the script execution...'
-  exit 1
-fi
+  env GOOS=$GOOS GOARCH=$GOARCH go build --ldflags='-s -w' -o $package_name *.go
+  upx --ultra-brute $package_name &>/dev/null
 
-zip $zip_filename $package_name
-rm $package_name
+  if [ $? -ne 0 ]; then
+    echo 'An error has occurred! Aborting the script execution...'
+    exit 1
+  fi
+
+  zip $zip_filename $package_name
+  rm $package_name
+done
